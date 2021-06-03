bd78f587cbadf19fc2e19247ea660b79ee8ebddb
fenglyu8 <tuyuxiai433@163.com>
2021-06-04 01:51:53 +0800

    MixedMessagesChapter7 project added

---
+++ MixedMessagesChapter7/src/B.java
@@ -0,0 +1,6 @@
+public class B extends A {
+    @Override
+    void m1() {
+        System.out.print("B's m1, ");
+    }
+}
---
+++ MixedMessagesChapter7/src/C.java
@@ -0,0 +1,6 @@
+public class C extends B {
+    @Override
+    void m3() {
+        System.out.print("C's m3, " + (ivar + 6));
+    }
+}
