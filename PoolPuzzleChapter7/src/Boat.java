9d0700309c1877da5e41e23aa9b0c458024077b0
fenglyu8 <tuyuxiai433@163.com>
2021-06-08 05:28:03 +0800

    PoolPuzzleChapter7 project added

---
+++ PoolPuzzleChapter7/src/Boat.java
@@ -0,0 +1,15 @@
+public class Boat {
+    private int lenght;
+
+    public void setLenght (int len) {
+        lenght = len;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void move() {
+        System.out.print("drift ");
+    }
+}
