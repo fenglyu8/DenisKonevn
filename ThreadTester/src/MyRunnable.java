8ffe67a869f6e63ad17f62a97fa86f5ba95249d9
fenglyu8 <tuyuxiai433@163.com>
2021-08-12 17:41:04 +0800

    ThreadTester project added

---
+++ ThreadTester/src/MyRunnable.java
@@ -0,0 +1,14 @@
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        go();
+    }
+
+    private void go() {
+        doMore();
+    }
+
+    private void doMore() {
+        System.out.println("top of the stack");
+    }
+}
