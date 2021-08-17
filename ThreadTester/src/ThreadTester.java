2f4d10da4adc50823542fdb73a60e6353b83949a
fenglyu8 <tuyuxiai433@163.com>
2021-08-17 14:31:54 +0800

    ThreadTester project updated

--- ThreadTester/src/ThreadTester.java
+++ ThreadTester/src/ThreadTester.java
@@ -1,11 +1,9 @@
 public class ThreadTester {
 
     public static void main(String[] args) {
-	    while (true) {
             Runnable threadJob = new MyRunnable();
             Thread myThread = new Thread(threadJob);
             myThread.start();
             System.out.println("back to main");
-        }
     }
 }
